b8b1dea8d44ac20f235568e5a3fa796b2cd0bc89
Joeleen Kennedy <joeleen@pressable.com>
2020-08-09 22:52:41 -0400

    JSONify jobs for resume

---
+++ src/components/Job.js
@@ -0,0 +1,72 @@
+import React from "react";
+import useDetectPrint from 'use-detect-print';
+
+/**
+ * Job component. Displays individual job item.
+ *
+ * @param {string} companyName Name of the company.
+ * @param {string} fullDate Full date of employment.
+ * @param {array} jobPositions Array of different positions held at the company.
+ *
+ * @returns article
+ */
+const Job = ({ companyName, fullDate, jobPositions }) => {
+  // Find out if we're printing.
+  const isPrinting = useDetectPrint();
+
+  // Get the company name and full date, if any.
+  const CompanyTitle = ({ companyName, fullDate }) => (
+    <h4>
+      {companyName}
+      {fullDate && <span className="job__date job__date--full">{fullDate}</span>}
+    </h4>
+  );
+
+  // Get job title(s) and employment date(s).
+  const JobMeta = ({ jobTitle, employmentDate }) => (
+    <>
+      <h5>{jobTitle}</h5>
+      <p className="job__date">{employmentDate}</p>
+    </>
+  );
+
+  // Get job description(s).
+  const JobDescription = ({ jobDescription }) => (
+    <p className="job__description">{jobDescription}</p>
+  );
+
+  // Get the full job details. If we're printing, group content in divs.
+  const jobFullDetails = jobPositions.map((jobPosition, index) =>
+    <React.Fragment key={index}>
+      {isPrinting ? (
+        <>
+          <div className="job--left">
+            {index === 0 && (
+              <CompanyTitle companyName={companyName} fullDate={fullDate} />
+            )}
+            <JobMeta {...jobPosition} />
+          </div>
+          <div className="job--right">
+            <JobDescription {...jobPosition} />
+          </div>
+        </>
+      ) : (
+        <>
+          {index === 0 && (
+            <CompanyTitle companyName={companyName} fullDate={fullDate} />
+          )}
+          <JobMeta {...jobPosition} />
+          <JobDescription {...jobPosition} />
+        </>
+      )}
+    </React.Fragment>
+  );
+
+  // Return the article.
+  return (
+    <article className="job">
+      {jobFullDetails}
+    </article>
+  );
+};
+export default Job;
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,6 +1,13 @@
 import React from "react";
+import jobsData from "../jobsData";
+import Job from "./Job";
 import PrintButton from "./PrintButton";
 import resume from "../files/JoeleenKennedyResume2019.pdf";
+
+// Map the jobs JSON data to a list of jobs.
+const jobsList = jobsData.jobsData.map((JobsDataItem, index) =>
+  <Job key={index} {...JobsDataItem} />
+);
 
 const Resume = () => (
   <section id="resume" className="resume">
@@ -26,99 +33,7 @@ const Resume = () => (
     </section>
     <section className="experience">
       <h3>Experience</h3>
-      <article className="pressable">
-        <h4>Pressable (Remote)</h4>
-        <h5>Concierge WordPress Engineer</h5>
-        <p className="date">4/18 - 2/19</p>
-        <p>
-          Contract with Automattic’s Special Projects team to design and build
-          custom sites for influential people and organizations, using
-          WordPress.com and hosted WordPress instances.
-        </p>
-      </article>
-      <article className="freelance">
-        <h4>Freelance</h4>
-        <h5>Front End Developer</h5>
-        <p className="date">9/14 - 3/18</p>
-        <p>
-          Provide web services for agencies, small businesses and nonprofits.
-        </p>
-      </article>
-      <article className="nlc">
-        <h4>National League of Cities</h4>
-        <h5>Manager, Web Development, Content and Analytics</h5>
-        <p className="date">9/13 - 9/14</p>
-        <p>
-          Manage NLC overall web presence. Design and develop new features and
-          multimedia projects. Plan and implement a site refresh/responsive
-          design overhaul. Coordinate, redesign, and rebuild all NLC email
-          marketing templates.
-        </p>
-      </article>
-      <article className="matrix">
-        <h4>
-          Matrix Group International, Inc{" "}
-          <span className="date full-date">(9/10 - 9/13)</span>
-        </h4>
-
-        <h5>User Experience Lead/Senior Front End Developer</h5>
-        <p className="date">12/11 - 9/13</p>
-        <p>
-          Unify the designers, front end developers, and information architect,
-          setting standards and championing the team. Review wireframes and
-          designs from a technical viewpoint, implement designs in CMSs,
-          including Sitefinity, Expression Engine and WordPress. Provide
-          consultations, train clients, teach workshops and co-present webinars.
-        </p>
-        <h5>Designer/Front End Developer</h5>
-        <p className="date">9/10 - 12/11</p>
-        <p>
-          Participate in client meetings, design wireframes, moodboards, web
-          comps, and style guides. Bridge design and development teams.
-          Implement designs into CMSs, including WordPress, Expression Engine,
-          CommonSpot, and a custom ColdFusion CMS. Lead client CMS training.
-        </p>
-      </article>
-      <article className="elon">
-        <h4>Elon University</h4>
-        <h5>Multimedia/Graphic Designer</h5>
-        <p className="date">11/08 - 9/10</p>
-        <p>
-          Support university departments with web sites, blogs and wikis.
-          Establish and maintain WordPress multisite installation. Create
-          WordPress themes and instructional animations. Print design for
-          posters, brochures, logos, shirts, etc. Mentor students and teach
-          workshops.
-        </p>
-      </article>
-      <article className="gd">
-        <h4>Graphik Dimensions</h4>
-        <h5>Junior Web Designer</h5>
-        <p className="date">2/08 - 10/08</p>
-        <p>
-          Maintain pictureframes.com, e-promos, quarterly print catalog,
-          high-end photo editing.
-        </p>
-      </article>
-      <article className="freelance">
-        <h4>Freelance</h4>
-        <h5>Web/Multimedia Design</h5>
-        <p className="date">12/06 - 12/11</p>
-        <p>
-          Design/development of web sites, e-promos, ads, animations, and
-          presentations. Design logos, stationary packages, brochures, shirts,
-          etc. Educational/instructional drawings for UNCG.
-        </p>
-      </article>
-      <article className="cosmopro">
-        <h4>Cosmopro Group</h4>
-        <h5>Graphic/Web Designer</h5>
-        <p className="date">5/06 - 1/07</p>
-        <p>
-          Maintain pevonia.com, ads for spafinder.com, presentations, print
-          design for spa menus/flyers.
-        </p>
-      </article>
+      {jobsList}
     </section>
     <section className="education">
       <h3>Education</h3>
