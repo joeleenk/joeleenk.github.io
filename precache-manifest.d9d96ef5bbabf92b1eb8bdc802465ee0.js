d94923e3e5072d418d6e3edc7bc12c81dcc5cac8
Joeleen Kennedy <joeleen@pressable.com>
2020-08-09 22:54:18 -0400

    Updates

---
+++ precache-manifest.d9d96ef5bbabf92b1eb8bdc802465ee0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d9b8e1689aeb11bfb6532731f3ec3ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21f3cd368f1ac4c11c1f",
+    "url": "/static/css/main.72229ed3.chunk.css"
+  },
+  {
+    "revision": "703c22b68dbb51a42cfb",
+    "url": "/static/js/2.e82c3925.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e82c3925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21f3cd368f1ac4c11c1f",
+    "url": "/static/js/main.6e6a7ebd.chunk.js"
+  },
+  {
+    "revision": "7d34c6f2093f9fdf8f7f",
+    "url": "/static/js/runtime-main.d44ecfb1.js"
+  },
+  {
+    "revision": "26c8a3c6f837155b0f6f7a9482945bd9",
+    "url": "/static/media/JoeleenKennedyResume2019.26c8a3c6.pdf"
+  },
+  {
+    "revision": "bc93f00c39df8043f327a78318e409dc",
+    "url": "/static/media/RougeScript-Regular.bc93f00c.woff2"
+  },
+  {
+    "revision": "e07a61b6bb8f6d49af896d117e0b7eac",
+    "url": "/static/media/Varta-VariableFont_wght.e07a61b6.woff2"
+  },
+  {
+    "revision": "0c87c5df8c3aebeb877dacd6281a9108",
+    "url": "/static/media/noise-texture.0c87c5df.png"
+  },
+  {
+    "revision": "8138e8ce251a8c0dd5329738f9b9f0e4",
+    "url": "/static/media/tigger.8138e8ce.jpg"
+  }
+]);
