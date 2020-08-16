0819047e3754931ae665fc2fcc4c4eda7f891104
Joeleen Kennedy <joeleen@pressable.com>
2020-08-16 19:59:45 -0400

    Trim down client list

--- src/components/Work.js
+++ src/components/Work.js
@@ -15,14 +15,14 @@ const Work = () => (
     <h3>Featured Projects</h3>
     <Projects projectsListData={workData.workData.projects} />
 
-    <h3>Clients I've worked with</h3>
+    <h3>Some of my clients</h3>
     <Clients clientsListData={workData.workData.clients} />
 
     <h3>Code samples</h3>
     <p>
-      <a href="https://github.com/joeleenk">I'm on GitHub!</a> Currently most of my work is hidden away in private repositories, but the source for this site lives there. <a title="Email Joeleen" href="mailto:joeleenk@gmail.com">Please let me know</a> if you'd like to see another code sample, and I'll do my best to accommodate. &#9786;
+      <a href="https://github.com/joeleenk">I'm on GitHub!</a> Currently most of my work is hidden away in private repositories, but the source for this site lives there. <a title="Email Joeleen" href="mailto:joeleenk@gmail.com">Please let me know</a> if you'd like to see another code sample, and I'll do my best to accommodate.
     </p>
-    <p>In the meantime, here's a picture of my cat:<br />
+    <p>In the meantime, here's a picture of my cat. &#9786;<br />
     <img src={tigger} className="tigger" alt="An elderly gray and white tabby cat named Tigger sleeping in a circle." />
     </p>
   </section>
